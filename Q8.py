79a9f26f3fd862bc6da0ef59d12c8d3160edc4c6
aaku123-gif <bhogalaakriti@gmail.com>
2024-06-18 01:59:21 +0530

    Add files via upload

---
+++ Q8.py
@@ -0,0 +1,16 @@
+#program that concatenates two strings and returns the result
+
+def concatenate_strings(str1, str2):
+
+  return str1 + str2
+
+
+if __name__ == "__main__":
+  str1 = "very"
+  str2 = "good"
+
+  result = concatenate_strings(str1, str2)
+
+  print(result)
+
+  
