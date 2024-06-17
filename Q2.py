f739e95a2a409252a268c948ebab927d49ec9e43
aaku123-gif <bhogalaakriti@gmail.com>
2024-06-18 00:08:30 +0530

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,9 @@
+def check_even_odd(num):
+    if num % 2 == 0:
+        print(f"{num} is even.")
+    else:
+        print(f"{num} is odd.")
+
+number = int(input("Enter a number: "))
+check_even_odd(number)
+
