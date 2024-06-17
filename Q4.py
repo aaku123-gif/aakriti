1c652b53290a52406067c6abbe375d53f20bf639
aaku123-gif <bhogalaakriti@gmail.com>
2024-06-18 01:01:32 +0530

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,8 @@
+#program that asks the user for their name and then prints a greeting message.
+
+# Ask for user's name
+name = input("What is your name? ")
+
+# Print a greeting message
+print(f"Hello, {name.title()} ")
+
