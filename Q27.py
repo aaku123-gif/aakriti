de93633c3074923b5b134c23c195dddeb9351082
aaku123-gif <bhogalaakriti@gmail.com>
2024-06-19 02:35:42 +0530

    Add files via upload

---
+++ Q27.py
@@ -0,0 +1,8 @@
+#this program converts a string into a list of its characters
+
+def string_to_list(string):
+    return list(string)
+
+input_string = "Namaste! India"
+characters_list = string_to_list(input_string)
+print(characters_list)
