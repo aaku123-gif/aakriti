9a4a26874d321ee9a631baf66ef16fd70237d7a6
aaku123-gif <bhogalaakriti@gmail.com>
2024-06-19 01:46:35 +0530

    Add files via upload

---
+++ Q17.py
@@ -0,0 +1,8 @@
+#this program converts a given string to title case (first letter of each word capitalized).
+
+def to_title_case(s):
+    return s.title()
+
+input_string = "i love python very much"
+title_case_string = to_title_case(input_string)
+print(title_case_string)
